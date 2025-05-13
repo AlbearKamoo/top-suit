0a3a5f63f978fdfc4f2bef6415d5e20ca9fa997c
Marcelo Autran <mrautran@gmail.com>
2025-05-13 18:28:32 -0400

    Remove dist

--- shared/dist/types.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SUITS = void 0;
-exports.SUITS = {
-    DIAMONDS: "♦",
-    HEARTS: "♥",
-    SPADES: "♠",
-    CLUBS: "♣",
-};
